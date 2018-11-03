4f96a6cd21edd04a4aeee2780cffec9b9f991c48
Clorthe44 <44092778+Clorthe44@users.noreply.github.com>
2018-11-03 19:40:48 -0400

    Update GameStart.m

---
+++ GameStart.m
@@ -0,0 +1,12 @@
+play = char(inputdlg("Do you want to play 60 Miles of Hell!!! (y for yes, any other key for no)", 'Start Game'))
+
+if (play =="y" || play == "Y"
+    gamestart = true
+    GameBoardDetails
+else
+    msgbox({'Game over' , 'Thank you for playing'})
+    close all hidden
+end
+    
+
+    
