0711c923f1c2573f3bdb8fa406ec0f84acb6b69d
Brianna <brianna.raphino@gmail.com>
2018-11-03 19:06:17 -0400

    Update dropitems.m

---
+++ dropitems.m
@@ -0,0 +1,16 @@
+function dropitems(level)
+switch level
+    case 1 
+          itemListW= itemListW1
+          itemListF=  itemListF1
+    case 2
+        itemListW= itemListW2
+        itemListF= itemListF2
+    case 3
+       itemListW= itemListW3
+       itemListF=  itemListF3
+end
+drrow = randi(1,100
+end
+
+       
