d9197a812e0f54b1430f73727083e5a27c9f69b9
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-03 20:01:44 -0400

    Update GameStart.m

--- GameStart.m
+++ GameStart.m
@@ -1,8 +1,8 @@
 play = char(inputdlg("Do you want to play 60 Miles of Hell!!! (y for yes, any other key for no)", 'Start Game'))
 
-if (play =="y" || play == "Y"
+if (play =="y" || play == "Y")
     gamestart = true
-    GameBoardDetails
+    GameboardDetails
 else
     msgbox({'Game over' , 'Thank you for playing'})
     close all hidden
