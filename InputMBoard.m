972b6c12a0afdafd283c11898ac48a17a21ff363
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-03 22:08:59 -0400

    w

--- InputMBoard.m
+++ InputMBoard.m
@@ -1,8 +1,8 @@
 function Mboard = InputMBoard(itemName, row, col, board)
 %INPUTMBOARD  will be called whenever someone needs to add a item placed on
-%Mboard
-itemID = str2double(itemName);
-board{row, col} = itemID;
+%INPUT THE item name in '' and CHECK WHETHER TOU CAN PLACE ITEM BEFORE EXE
+itemID = sum(double(itemName));
+board(row, col) = itemID;
 Mboard = board;
 end
 
