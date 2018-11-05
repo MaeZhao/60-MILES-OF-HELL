108ad8ddb1318be214acaa15f3bde98d80b54d1f
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-04 23:45:39 -0500

    Update GameStart.m
    Added a story

--- GameStart.m
+++ GameStart.m
@@ -3,6 +3,10 @@ clc
 play = char(inputdlg("Do you want to play 60 Miles of Hell!!! (y for yes, any other key for no)", 'Start Game'))
 if (play =="y" || play == "Y")
     gamestart = true;
+    player = imread('All_Sprites_PNG\temp_chick.png');
+    start = msgbox('Chicken lil is lost in the woods. The nearest civilization is 60 miles away. Help in his journey to saftey!', 'The story!', 'custom',...
+        player)
+    waitfor(start);
     GameboardDetails
 else
     msgbox({'Game over'})
