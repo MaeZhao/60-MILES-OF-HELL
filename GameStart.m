8d21bdb99c0fc511a421e9b96d7fbbdde51d34b9
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-05 08:08:49 -0500

    Update GameStart.m

--- GameStart.m
+++ GameStart.m
@@ -4,7 +4,7 @@ play = char(inputdlg("Do you want to play 60 Miles of Hell!!! (y for yes, any ot
 if (play =="y" || play == "Y")
     gamestart = true;
     player = imread('All_Sprites_PNG\temp_chick.png');
-    start = msgbox('Chicken lil is lost in the woods. The nearest civilization is 60 miles away. Help in his journey to saftey!', 'The story!', 'custom',...
+    start = msgbox('Chicken lil is lost in the woods. The nearest civilization is 60 miles away. Help him in his journey to saftey!', 'The story!', 'custom',...
         player)
     waitfor(start);
     GameboardDetails
