a739e46efdf17b3a6a9479c23a2e7a98b4ac68f9
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-08 22:23:18 -0500

    npc move

--- NPCMovement.m
+++ NPCMovement.m
@@ -1,3 +1,13 @@
 % needs to happen with EVERY single level
-if (level = 1)
-    find(
+function [Mboard, Gboard] = NPCMovement(npcListL1, NPCList, Mboard, Gboard)
+for i=1: length(npcListL1)
+   tempNPCID(i) = sum(double(char(npcListL1(i))));
+end
+temp D
+if (level == 1)
+    for d=1; length(npcListL1)
+        find(Mboard,tempNPCID(d))
+    end 
+end 
+        
+    
