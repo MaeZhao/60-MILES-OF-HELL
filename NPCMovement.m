52cd19d512f597f40ec2923d6ba5db814d2b62b0
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-08 23:49:28 -0500

    NPC MOVEMENT (TBIMPLEMENTED)

--- NPCMovement.m
+++ NPCMovement.m
@@ -1,11 +1,10 @@
 % needs to happen with EVERY single level
-function [Mboard, Gboard] = NPCMovement(NPCIDList, NPCFreq,NPCName, mboard, gboard)
+function [Mboard, Gboard] = NPCMovement(NPCIDList,NPCName,NPCPic, mboard, gboard)
 tboard = mboard;
 if (level == 1)
     for d=1; length(NPCIDList)
         find(tboard,NPCIDList(d))
-        % [Gboard, Mboard] = placeItem(mboard, gboard, Itemname, Itemtype, Itemfreq)
-        [mboard, gboard] = placeItem(mboard, gboard, NPCName(d), 
+        [mboard, gboard] = placeItem(mboard, gboard, NPCName(d),NPCPic,1)
     end 
 end 
         
