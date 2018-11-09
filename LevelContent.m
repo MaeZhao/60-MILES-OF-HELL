8122a36cdb70fa0c9e1a6e61211545dcfdc1e1ea
Clorthe44 <44092778+Clorthe44@users.noreply.github.com>
2018-11-08 23:27:43 -0500

    UPDATED SHIT

--- LevelContent.m
+++ LevelContent.m
@@ -1,15 +1,22 @@
 %% Level Content 
-
-
+clear
+clc
 if (level == 1)
     itemListW = ["spikyNut", "phoneFlash", "keys", "stick"]  
     itemListF = ["funGuy",  "crumbs", "apple"]
+    itemFHunger = [3, 4, 15 ; 6, 4, 15]
     npcList= ["snakes","hornets","spiders","megaToad"]
     npcHP = [6,3,2,10]
     npcATk = [-5,-2,-2,-8]
     npcID = zeros(1,length(npcList))
     for i = 1:length(npcList)
-    npcID(i)= sum(double(char(npcListL1(i))))
+    npcID(i)= sum(double(char(npcList(i))))   
+    end
+    for i = 1:length(itemListW)
+    ItemWID(i)= sum(double(char(itemListW(i))))
+    end
+    for i = 1:length(itemListF)
+    ItemFID(i)= sum(double(char(itemListF(i))))
     end
     npcFreq= [3,3,3,1]
 
@@ -23,8 +30,14 @@ if (level == 2)
     npcATk= [-6,-5,-3,-10]
     npcID = zeros(1,length(npcList))
     for i = 1:length(npcList)
-    npcID(i)= sum(double(char(npcListL1(i))))
+    npcID(i)= sum(double(char(npcList(i))))
     end
+    for i = 1:length(itemListW)
+    ItemWID(i)= sum(double(char(itemListW(i))))
+    end
+for i = 1:length(itemListF)
+    ItemFID(i)= sum(double(char(itemListF(i))))
+end
     npcFreq=[4,4,2,1]
 end
 
@@ -35,11 +48,15 @@ if (level == 3)
     npcHP= [6,3,10,20]
     npcATk= [-7,-3,-5,-15]
     npcID = zeros(1,length(npcList))
-    for i = 1:length(npcList)
-    npcID(i)= sum(double(char(npcListL1(i))))
+   for i = 1:length(npcList)
+    npcID(i)= sum(double(char(npcList(i))))
     end
+    for i = 1:length(itemListW)
+    ItemWID(i)= sum(double(char(itemListW(i))))
+    end
+for i = 1:length(itemListF)
+    ItemFID(i)= sum(double(char(itemListF(i))))
+end
     npcFreq= [4,6,3,1]
-    
-
 
 end 
