a888f5582bba75b97042b2f5bb871a9ce88ee179
MaeZhao <39135372+MaeZhao@users.noreply.github.com>
2018-11-27 17:21:12 -0500

    used the / instead of \ for file search

--- GameStart.m
+++ GameStart.m
@@ -6,7 +6,7 @@ play = questdlg("Do you want to play?",'60MILES', "Yes",...
     "No", "Yes")
 switch play
     case "Yes"
-    player = imread('All_Sprites_PNG\temp_chick.png');
+    player = imread('All_Sprites_PNG/temp_chick.png');
     start = msgbox('Chicken lil is lost in the woods. The nearest civilization is 60 miles away. Help him in his journey to saftey!', 'The story!', 'custom',...
         player)
     waitfor(start);
--- loadSprite.m
+++ loadSprite.m
@@ -6,21 +6,21 @@ function [itemPicList] = loadSprite(itemList,type, level)
 if(type == 'W')
     itemPicList = cell(1, size(itemList,2))
     for h=1:length(itemList)
-        address = "All_Sprites_PNG\weapon\Weapon_"
+        address = "All_Sprites_PNG/weapon/Weapon_"
         itemPicList{h} = imread(strcat(address,string(level),"_", string(h),".png"));
     end
     
 elseif(type == 'F')
     itemPicList = cell(1, size(itemList,2))
     for h=1:length(itemList)
-        address = "All_Sprites_PNG\food\Food_"
+        address = "All_Sprites_PNG/food/Food_"
         itemPicList{h} = imread(strcat(address,string(level),"_", string(h),".png"));
     end
     
 elseif(type == 'N')
     itemPicList = cell(1, size(itemList,2))
     for h=1:length(itemList)
-        address = "All_Sprites_PNG\npc\npc_" %naming convention
+        address = "All_Sprites_PNG/npc/npc_" %naming convention
         itemPicList{h} = imread(strcat(address,string(level),"_", string(h),".png"));
     end
 end
